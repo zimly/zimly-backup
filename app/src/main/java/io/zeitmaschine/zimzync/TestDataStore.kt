ca230a0c453cd99f307ba5c5b27f4cf0c56ece61
Espen Jervidalo <espen.jervidalo@gmail.com>
2022-08-16 16:11:01 +0200

    Create a serializer and context bound datastore for protobuf object
    https://developer.android.com/topic/libraries/architecture/datastore#proto-create

---
+++ app/src/main/java/io/zeitmaschine/zimzync/TestDataStore.kt
@@ -0,0 +1,33 @@
+package io.zeitmaschine.zimzync
+
+import android.content.Context
+import androidx.datastore.core.CorruptionException
+import androidx.datastore.core.DataStore
+import androidx.datastore.core.Serializer
+import androidx.datastore.dataStore
+import com.google.protobuf.InvalidProtocolBufferException
+import java.io.InputStream
+import java.io.OutputStream
+
+object TestSerializer : Serializer<Test> {
+    override val defaultValue: Test = Test.getDefaultInstance()
+
+    override suspend fun readFrom(input: InputStream): Test {
+        try {
+            return Test.parseFrom(input)
+        } catch (exception: InvalidProtocolBufferException) {
+            throw CorruptionException("Cannot read proto.", exception)
+        }
+    }
+
+    override suspend fun writeTo(
+        t: Test,
+        output: OutputStream
+    ) = t.writeTo(output)
+}
+
+val Context.testDataStore: DataStore<Test> by dataStore(
+    fileName = "test.pb",
+    serializer = TestSerializer
+)
+
