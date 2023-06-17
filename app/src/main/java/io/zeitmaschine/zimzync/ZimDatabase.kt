2ed388347c58d8f339985230875f012d47d86ceb
Espen Jervidalo <espen.jervidalo@gmail.com>
2023-06-17 15:17:27 +0200

    Enable schema migration for room DB

--- app/src/main/java/io/zeitmaschine/zimzync/ZimDatabase.kt
+++ app/src/main/java/io/zeitmaschine/zimzync/ZimDatabase.kt
@@ -3,7 +3,11 @@ package io.zeitmaschine.zimzync
 import androidx.room.Database
 import androidx.room.RoomDatabase
 
-@Database(entities = [Remote::class], version = 1)
+@Database(
+    entities = [Remote::class],
+    version = 1,
+    autoMigrations = []
+)
 abstract class ZimDatabase : RoomDatabase() {
     abstract fun remoteDao(): RemoteDao
 }
