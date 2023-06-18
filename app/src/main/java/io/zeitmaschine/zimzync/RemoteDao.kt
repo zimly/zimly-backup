bf81387236706cde74545d2582bf57e67adbc38f
Espen Jervidalo <espen.jervidalo@gmail.com>
2023-06-18 14:37:04 +0200

    Revert "Add model for sync logs"
    This reverts commit 2f8172a06263a561098f770928c0badc057dfe5a.

--- app/src/main/java/io/zeitmaschine/zimzync/RemoteDao.kt
+++ app/src/main/java/io/zeitmaschine/zimzync/RemoteDao.kt
@@ -14,12 +14,6 @@ interface RemoteDao {
     @Query("SELECT * FROM remote WHERE uid = :remoteId")
     suspend fun loadById(remoteId: Int): Remote
 
-    @Query("SELECT * FROM remote JOIN log ON remote.uid = log.remote_id WHERE remote.uid = :remoteId")
-    fun loadRemoteAndLogs(remoteId: Int): Map<Remote, List<Log>>
-
-    @Query("SELECT CASE WHEN EXISTS (SELECT * FROM remote JOIN log ON remote.uid = log.remote_id WHERE remote.uid = :remoteId AND log.status = 'IN_PROGRESS') THEN CAST(1 AS BIT) ELSE CAST(0 AS BIT) END")
-    fun loadRemoteInProgress(remoteId: Int): Boolean
-
     @Delete
     suspend fun delete(remote: Remote)
 
--- app/src/main/java/io/zeitmaschine/zimzync/ZimDatabase.kt
+++ app/src/main/java/io/zeitmaschine/zimzync/ZimDatabase.kt
@@ -1,18 +1,13 @@
 package io.zeitmaschine.zimzync
 
-import androidx.room.AutoMigration
 import androidx.room.Database
 import androidx.room.RoomDatabase
-import androidx.room.TypeConverters
 
 @Database(
-    entities = [Remote::class, Log::class],
-    version = 2,
-    autoMigrations = [
-        AutoMigration(from = 1, to = 2)
-    ]
+    entities = [Remote::class],
+    version = 1,
+    autoMigrations = []
 )
-@TypeConverters(Converters::class)
 abstract class ZimDatabase : RoomDatabase() {
     abstract fun remoteDao(): RemoteDao
 }
