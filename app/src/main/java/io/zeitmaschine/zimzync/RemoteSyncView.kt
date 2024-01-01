8bc071aa470077c5c939ced91a6905f2efd9e5b8
Espen Jervidalo <espen.jervidalo@gmail.com>
2024-01-01 18:18:36 +0100

    Add card for remote configuration

--- app/src/main/java/io/zeitmaschine/zimzync/RemoteSyncView.kt
+++ app/src/main/java/io/zeitmaschine/zimzync/RemoteSyncView.kt
@@ -3,11 +3,17 @@ package io.zeitmaschine.zimzync
 import android.app.Application
 import android.util.Log
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.outlined.CloudUpload
 import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -29,6 +35,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalLifecycleOwner
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -333,14 +340,39 @@ private fun SyncCompose(
             modifier = Modifier.padding(all = 16.dp) then Modifier.padding(
                 top = innerPadding.calculateTopPadding(),
                 bottom = innerPadding.calculateBottomPadding()
-            ),
-            verticalArrangement = Arrangement.spacedBy(8.dp),
+            ) then Modifier.fillMaxWidth(),
+            //verticalArrangement = Arrangement.spacedBy(8.dp),
         ) {
-            Text(state.value.name)
-            Text(state.value.url)
-            Text(state.value.bucket)
-            Text(state.value.key)
-            Text(state.value.secret)
+            Card(
+                colors = CardDefaults.cardColors(
+                    containerColor = MaterialTheme.colorScheme.surfaceVariant,
+                ),
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                Box(contentAlignment = Alignment.TopEnd, modifier = Modifier.fillMaxWidth()) {
+                    Icon(
+                        Icons.Outlined.CloudUpload,
+                        "Remote",
+                        modifier = Modifier.padding(top = 8.dp, end = 8.dp)
+                    )
+                }
+                Column(modifier = Modifier.padding(16.dp)) {
+                    Row(
+                        modifier = Modifier.fillMaxWidth(),
+                        horizontalArrangement = Arrangement.SpaceBetween
+                    ) {
+                        Text(text = "URL", textAlign = TextAlign.Left)
+                        Text(state.value.url, textAlign = TextAlign.Right)
+                    }
+                    Row(
+                        modifier = Modifier.fillMaxWidth(),
+                        horizontalArrangement = Arrangement.SpaceBetween
+                    ) {
+                        Text(text = "Bucket", textAlign = TextAlign.Left)
+                        Text(state.value.bucket)
+                    }
+                }
+            }
             Text(state.value.folder)
             Text("Remotes: ${state.value.diff.remotes.size}")
             Text("Locales: ${state.value.diff.locals.size}")
