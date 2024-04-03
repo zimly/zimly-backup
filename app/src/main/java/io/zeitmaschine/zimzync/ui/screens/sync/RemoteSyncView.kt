5f93d9448425880a3e84e778975bb0b96c16a507
Espen Jervidalo <espen.jervidalo@gmail.com>
2024-04-03 17:14:58 +0200

    Extract sync ViewModel

--- app/src/main/java/io/zeitmaschine/zimzync/ui/screens/sync/RemoteSyncView.kt
+++ app/src/main/java/io/zeitmaschine/zimzync/ui/screens/sync/RemoteSyncView.kt
@@ -2,7 +2,6 @@ package io.zeitmaschine.zimzync.ui.screens.sync
 
 import android.app.Application
 import android.text.format.Formatter
-import android.util.Log
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -35,7 +34,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.snapshotFlow
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
@@ -43,247 +41,15 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.lifecycle.viewModelScope
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
-import androidx.work.Constraints
-import androidx.work.ExistingWorkPolicy
-import androidx.work.NetworkType
-import androidx.work.OneTimeWorkRequestBuilder
-import androidx.work.WorkInfo
-import androidx.work.WorkManager
-import androidx.work.WorkQuery
-import androidx.work.workDataOf
-import io.zeitmaschine.zimzync.Diff
-import io.zeitmaschine.zimzync.MediaRepository
-import io.zeitmaschine.zimzync.MinioRepository
 import io.zeitmaschine.zimzync.RemoteDao
-import io.zeitmaschine.zimzync.ResolverBasedRepository
-import io.zeitmaschine.zimzync.SyncInputs
-import io.zeitmaschine.zimzync.SyncOutputs
-import io.zeitmaschine.zimzync.SyncServiceImpl
-import io.zeitmaschine.zimzync.SyncWorker
 import io.zeitmaschine.zimzync.ui.theme.ZimzyncTheme
 import io.zeitmaschine.zimzync.ui.theme.containerBackground
-import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.SharingStarted
-import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.filter
-import kotlinx.coroutines.flow.filterNotNull
-import kotlinx.coroutines.flow.flatMapLatest
-import kotlinx.coroutines.flow.map
-import kotlinx.coroutines.flow.merge
-import kotlinx.coroutines.flow.onEach
-import kotlinx.coroutines.flow.stateIn
-import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
-import java.util.UUID
-
-@OptIn(ExperimentalCoroutinesApi::class)
-class SyncModel(private val dao: RemoteDao, private val remoteId: Int, application: Application) :
-    AndroidViewModel(application) {
-
-    // Todo: https://luisramos.dev/testing-your-android-viewmodel
-    companion object {
-        val TAG: String? = SyncModel::class.simpleName
-    }
-
-    // This identifier is used to identify already running sync instances and prevent simultaneous
-    // sync-executions.
-    private var uniqueWorkIdentifier = "sync_${remoteId}"
-
-    private val workManager by lazy { WorkManager.getInstance(application.applicationContext) }
-    private val contentResolver by lazy { application.contentResolver }
-
-    private val mediaRepo: MediaRepository = ResolverBasedRepository(contentResolver)
-
-    var remoteState: Flow<RemoteState> = snapshotFlow { remoteId }
-        .map { dao.loadById(it) }
-        .map {
-            RemoteState(
-                name = it.name,
-                url = it.url,
-                bucket = it.bucket,
-                folder = it.folder
-            )
-        }
-
-    var folderState = remoteState.map {
-        val photoCount = mediaRepo.getPhotos(setOf(it.folder)).size
-        val videoCount = mediaRepo.getVideos(setOf(it.folder)).size
-        return@map FolderState(it.folder, photoCount, videoCount)
-    }
-
-    // Flow created when starting the sync
-    private val _startedSyncId: MutableStateFlow<UUID?> = MutableStateFlow(null)
-
-    // Flow for already running sync jobs
-    private val _runningSyncId: Flow<UUID?> = snapshotFlow { loadSyncState() }
-
-    private val _observedProgress = merge(_runningSyncId, _startedSyncId)
-        .onEach { Log.i(TAG, "syncId: $it") }
-        .filterNotNull()
-        .flatMapLatest { observeSyncProgress(it) }
-
-    private val _diff: MutableStateFlow<Diff?> = MutableStateFlow(null)
-    private val _diffProgress: Flow<Progress> = _diff.filterNotNull().map { Progress(diffBytes = it.size, diffCount = it.diff.size)  }
-
-    // merge the flows from observed progress and one time diff
-    var progressState: StateFlow<Progress> = merge(_observedProgress, _diffProgress)
-            .stateIn(
-                scope = viewModelScope,
-                started = SharingStarted.WhileSubscribed(5_000),
-                initialValue = Progress(),
-            )
-
-    // mutable error flow, for manually triggered errors
-    private val _error: MutableStateFlow<String?> = MutableStateFlow(null)
-
-    // Merge errors from progress and manually triggered errors into one observable for the UI
-    // TODO there's still a corner case where _error doesn't emit?
-    val error: StateFlow<String?> = merge(_error, progressState.map { it.error })
-        .stateIn(
-            scope = viewModelScope,
-            started = SharingStarted.WhileSubscribed(5_000),
-            initialValue = null,
-        )
-
-    suspend fun createDiff() {
-
-        val remote = dao.loadById(remoteId)
-        val s3Repo = MinioRepository(remote.url, remote.key, remote.secret, remote.bucket)
-        val syncService = SyncServiceImpl(s3Repo, mediaRepo)
-        // Display result of the minio request to the user
-        try {
-            val diff = syncService.diff(setOf(remote.folder))
-            _diff.update { diff }
-        } catch (e: Exception) {
-            _error.update { e.message ?: "Unknown error." }
-        }
-    }
-
-    // TODO https://code.luasoftware.com/tutorials/android/jetpack-compose-load-data
-    private fun loadSyncState(): UUID? {
-        val query = WorkQuery.Builder
-            .fromUniqueWorkNames(listOf(uniqueWorkIdentifier))
-            .addStates(listOf(WorkInfo.State.RUNNING))
-            .build()
-        val running = workManager.getWorkInfos(query).get()
-
-        return when (running.size) {
-            0 -> null // empty
-            1 -> running.first().id
-            else -> throw Error("More than one unique sync job in progress. This should not happen.")
-        }
-    }
-
-    private fun observeSyncProgress(id: UUID): Flow<Progress> {
-        return workManager.getWorkInfoByIdFlow(id)
-            .filterNotNull()
-            // filter out unhandled states that result in a "nulled" out progress object.
-            .filter { it.state in arrayOf(WorkInfo.State.SUCCEEDED, WorkInfo.State.RUNNING, WorkInfo.State.FAILED) }
-            .map { workInfo ->
-
-            val progressState = Progress()
-
-            when (workInfo.state) {
-                WorkInfo.State.RUNNING -> {
-                    val progress = workInfo.progress
-                    progressState.progressCount = progress.getInt(SyncOutputs.PROGRESS_COUNT, 0)
-                    progressState.progressBytes = progress.getLong(SyncOutputs.PROGRESS_BYTES, 0)
-                    progressState.percentage = progress.getFloat(SyncOutputs.PROGRESS_PERCENTAGE, 0F)
-                    progressState.diffCount = progress.getInt(SyncOutputs.DIFF_COUNT, 0)
-                    progressState.diffBytes = progress.getLong(SyncOutputs.DIFF_BYTES, 0)
-                    progressState.inProgress = true
-                }
-
-                WorkInfo.State.SUCCEEDED -> {
-                    val output = workInfo.outputData
-                    progressState.progressCount = output.getInt(SyncOutputs.PROGRESS_COUNT, 0)
-                    progressState.progressBytes = output.getLong(SyncOutputs.PROGRESS_BYTES, 0)
-                    progressState.percentage = output.getFloat(SyncOutputs.PROGRESS_PERCENTAGE, 0F)
-                    progressState.diffCount = output.getInt(SyncOutputs.DIFF_COUNT, 0)
-                    progressState.diffBytes = output.getLong(SyncOutputs.DIFF_BYTES, 0)
-                    progressState.inProgress = false
-                }
-
-                WorkInfo.State.FAILED -> {
-                    val output = workInfo.outputData
-                    progressState.progressCount = output.getInt(SyncOutputs.PROGRESS_COUNT, 0)
-                    progressState.progressBytes = output.getLong(SyncOutputs.PROGRESS_BYTES, 0)
-                    progressState.percentage = output.getFloat(SyncOutputs.PROGRESS_PERCENTAGE, 0F)
-                    progressState.diffCount = output.getInt(SyncOutputs.DIFF_COUNT, 0)
-                    progressState.diffBytes = output.getLong(SyncOutputs.DIFF_BYTES, 0)
-                    progressState.error = output.getString(SyncOutputs.ERROR) ?: "Unknown error."
-                    progressState.inProgress = false
-                }
-
-                else -> {
-                    Log.e(TAG, "State '${workInfo.state}' should not be observed.")
-                }
-            }
-            progressState
-        }
-    }
-
-    // TODO check error handling!
-    suspend fun sync(): UUID {
-        val remote = dao.loadById(remoteId)
-        val data = workDataOf(
-            SyncInputs.S3_URL to remote.url,
-            SyncInputs.S3_KEY to remote.key,
-            SyncInputs.S3_SECRET to remote.secret,
-            SyncInputs.S3_BUCKET to remote.bucket,
-            SyncInputs.DEVICE_FOLDER to arrayOf(remote.folder)
-        )
-        val constraints = Constraints.Builder()
-            .setRequiredNetworkType(NetworkType.CONNECTED)
-            .setRequiresBatteryNotLow(true)
-            .build()
-
-        val syncRequest = OneTimeWorkRequestBuilder<SyncWorker>()
-            .setInputData(data)
-            .setConstraints(constraints)
-            .build()
-
-        workManager.enqueueUniqueWork(uniqueWorkIdentifier, ExistingWorkPolicy.KEEP, syncRequest)
-        _startedSyncId.update { syncRequest.id }
-
-        return syncRequest.id
-    }
-
-    fun clearError() {
-        _error.update { null }
-    }
-
-    data class RemoteState(
-        var name: String = "",
-        var url: String = "",
-        var bucket: String = "",
-        var folder: String = "",
-    )
-
-    data class FolderState(
-        var folder: String = "",
-        var photos: Int = 0,
-        var videos: Int = 0,
-    )
-
-    data class Progress(
-        var percentage: Float = 0.0f,
-        var progressCount: Int = 0,
-        var progressBytes: Long = 0,
-        var diffCount: Int = 0,
-        var diffBytes: Long = 0,
-        var inProgress: Boolean = false,
-        var error: String = "",
-    )
-}
 
 @Composable
 fun SyncRemote(
@@ -292,16 +58,16 @@ fun SyncRemote(
     application: Application,
     edit: (Int) -> Unit,
     back: () -> Unit,
-    viewModel: SyncModel = viewModel(factory = viewModelFactory {
+    viewModel: SyncViewModel = viewModel(factory = viewModelFactory {
         initializer {
-            SyncModel(dao, remoteId, application)
+            SyncViewModel(dao, remoteId, application)
         }
     }),
 ) {
 
-    val remote by viewModel.remoteState.collectAsStateWithLifecycle(SyncModel.RemoteState())
+    val remote by viewModel.remoteState.collectAsStateWithLifecycle(SyncViewModel.RemoteState())
     val error by viewModel.error.collectAsStateWithLifecycle()
-    val folder by viewModel.folderState.collectAsStateWithLifecycle(SyncModel.FolderState())
+    val folder by viewModel.folderState.collectAsStateWithLifecycle(SyncViewModel.FolderState())
     val progress by viewModel.progressState.collectAsStateWithLifecycle()
 
     // want to go nuts?
@@ -329,10 +95,10 @@ fun SyncRemote(
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 private fun SyncCompose(
-    remote: SyncModel.RemoteState,
+    remote: SyncViewModel.RemoteState,
     error: String?,
-    folder: SyncModel.FolderState,
-    progress: SyncModel.Progress,
+    folder: SyncViewModel.FolderState,
+    progress: SyncViewModel.Progress,
     snackbarState: SnackbarHostState,
     sync: () -> Unit,
     createDiff: () -> Unit,
@@ -544,14 +310,14 @@ private fun SyncCompose(
 @Composable
 fun SyncPreview() {
 
-    val remote = SyncModel.RemoteState(
+    val remote = SyncViewModel.RemoteState(
         name = "zeitmaschine.io",
         url = "http://10.0.2.2:9000",
         bucket = "test-bucket",
         folder = "Camera"
     )
-    val progressState = SyncModel.Progress()
-    val folderState = SyncModel.FolderState()
+    val progressState = SyncViewModel.Progress()
+    val folderState = SyncViewModel.FolderState()
     val snackbarState = remember { SnackbarHostState() }
 
 
