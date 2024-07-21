b414ecda2e1001d3c41b57e4e9289977e3566e99
Espen Jervidalo <espen.jervidalo@gmail.com>
2024-07-21 20:13:07 +0200

    Chunk progress flow and complete earlier #5

--- app/src/main/java/io/zeitmaschine/zimzync/data/s3/ProgressTracker.kt
+++ app/src/main/java/io/zeitmaschine/zimzync/data/s3/ProgressTracker.kt
@@ -2,14 +2,16 @@ package io.zeitmaschine.zimzync.data.s3
 
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.runningFold
 import kotlinx.coroutines.flow.transformWhile
 import kotlin.time.TimeSource
 
 
-class ProgressTracker(private val size: Long) {
+class ProgressTracker(private val size: Long, private val chunkSize: Int = 5) {
 
     private val timeSource = TimeSource.Monotonic
+    private var totalRead = 0L
 
     // Important: MutableStateFlow does not emit same values!
     private val progressFlow = MutableSharedFlow<Long>(500)
@@ -20,36 +22,79 @@ class ProgressTracker(private val size: Long) {
         progressFlow.tryEmit(read.toLong())
     }
 
-    fun stepTo(skipped: Long) {
-        progressFlow.tryEmit(skipped)
-    }
-
     fun observe(): Flow<Progress> {
         return progressFlow
-            .runningFold(Progress.EMPTY) { acc, inc ->
-                val totalBytes = acc.totalReadBytes + inc
+            .transformWhile {
+                emit(it)
+                totalRead += it
+                totalRead < size
+            }
+            .chunked(chunkSize) // normalizing over chunks for better speed calculation
+            .runningFold(Progress.EMPTY) { acc, chunk ->
+                val readBytes = chunk.sum()
+                val totalBytes = acc.totalReadBytes + readBytes
                 val percentage = totalBytes.toFloat() / size
 
                 val timeMark = timeSource.markNow()
                 var duration = (timeMark - acc.timeMark).inWholeMicroseconds
 
-                // Divide by zero safe guard,
+                // Divide by zero safe guard
                 duration = if (duration > 0) duration else 1
 
                 // Bytes/µs to Bytes/s
-                val bytesPerSec = inc * 1000_000 / duration
-                Progress(inc, totalBytes, percentage, size, bytesPerSec, timeMark)
+                val bytesPerSec = readBytes * 1000_000 / duration
+                Progress(readBytes, totalBytes, percentage, size, bytesPerSec, timeMark)
             }
-            .transformWhile {
-                emit(it)
-                it.percentage < 1F
-            }
+
     }
 }
 
-data class Progress(val readBytes: Long, val totalReadBytes: Long, val percentage: Float, val size: Long, val bytesPerSec: Long, val timeMark: TimeSource.Monotonic.ValueTimeMark) {
+data class Progress(
+    val readBytes: Long,
+    val totalReadBytes: Long,
+    val percentage: Float,
+    val size: Long,
+    val bytesPerSec: Long,
+    val timeMark: TimeSource.Monotonic.ValueTimeMark
+) {
     companion object {
         val EMPTY = Progress(0, 0, 0F, 0, 0, TimeSource.Monotonic.markNow())
+    }
+}
+
+/**
+ * Taken from https://github.com/Kotlin/kotlinx.coroutines/pull/4127/files
+ *
+ * Splits the given flow into a flow of non-overlapping lists each not exceeding the given [size] but never empty.
+ * The last emitted list may have fewer elements than the given size.
+ *
+ * Example of usage:
+ * ```
+ * flowOf("a", "b", "c", "d", "e")
+ *     .chunked(2) // ["a", "b"], ["c", "d"], ["e"]
+ *     .map { it.joinToString(separator = "") }
+ *     .collect {
+ *         println(it) // Prints "ab", "cd", e"
+ *     }
+ * ```
+ *
+ * @throws IllegalArgumentException if [size] is not positive.
+ */
+fun <T> Flow<T>.chunked(size: Int): Flow<List<T>> {
+    require(size >= 1) { "Expected positive chunk size, but got $size" }
+    return flow {
+        var result: ArrayList<T>? = null // Do not preallocate anything
+        collect { value ->
+            // Allocate if needed
+            val acc = result ?: ArrayList<T>(size).also { result = it }
+            acc.add(value)
+            if (acc.size == size) {
+                emit(acc)
+                // Cleanup, but don't allocate -- it might've been the case this is the last element
+                result = null
+            }
+        }
+        result?.let { emit(it) }
     }
 }
 
