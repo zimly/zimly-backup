a2c0d82debf8329ad2f0a5aada4b1e78dd7f7b7c
Espen Jervidalo <espen.jervidalo@gmail.com>
2024-07-21 17:06:39 +0200

    Extract ProgressStream wrapper out #5

--- app/src/main/java/io/zeitmaschine/zimzync/data/s3/ProgressTracker.kt
+++ app/src/main/java/io/zeitmaschine/zimzync/data/s3/ProgressTracker.kt
@@ -8,41 +8,6 @@ import java.io.FilterInputStream
 import java.io.InputStream
 import kotlin.time.TimeSource
 
-/**
- * Wraps an Inputstream to track the progress. Inspired by https://github.com/minio/minio-java/blob/master/examples/ProgressStream.java
- */
-class ProgressStream(private val delegate: InputStream, private val progress: ProgressTracker) :
-    FilterInputStream(delegate) {
-    companion object {
-        fun wrap(delegate: InputStream, progress: ProgressTracker): InputStream =
-            ProgressStream(delegate, progress)
-    }
-
-    override fun read(): Int {
-        val readBytes = delegate.read()
-        this.progress.stepBy(readBytes)
-        return readBytes
-    }
-
-    override fun read(toStore: ByteArray?): Int {
-        val readBytes: Int = this.delegate.read(toStore)
-        this.progress.stepBy(readBytes)
-        return readBytes
-    }
-
-    override fun read(toStore: ByteArray?, off: Int, len: Int): Int {
-        val readBytes: Int = this.delegate.read(toStore, off, len)
-        this.progress.stepBy(readBytes)
-        return readBytes
-    }
-
-    override fun skip(n: Long): Long {
-        val readBytes = this.delegate.skip(n)
-        this.progress.stepTo(readBytes)
-        return readBytes
-    }
-
-}
 
 class ProgressTracker(private val size: Long) {
 
@@ -79,7 +44,6 @@ class ProgressTracker(private val size: Long) {
                 Progress(inc, totalBytes, percentage, size, bytesPerSec, timeMark)
             }
     }
-
 }
 
 data class Progress(val readBytes: Long, val totalReadBytes: Long, val percentage: Float, val size: Long, val bytesPerSec: Long, val timeMark: TimeSource.Monotonic.ValueTimeMark) {
@@ -87,3 +51,4 @@ data class Progress(val readBytes: Long, val totalReadBytes: Long, val percentag
         val EMPTY = Progress(0, 0, 0F, 0, 0, TimeSource.Monotonic.markNow())
     }
 }
+
