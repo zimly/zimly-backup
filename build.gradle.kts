e3bdf7caf49cd03629fba2b5740128eafa0980de
Espen Jervidalo <espen.jervidalo@gmail.com>
2024-10-13 14:13:41 +0200

    Migrate gradle to kotlin

---
+++ build.gradle.kts
@@ -0,0 +1,20 @@
+buildscript {
+    extra.apply {
+        // https://developer.android.com/jetpack/androidx/releases/compose
+        set("compose_version", "1.7.2")
+        // https://developer.android.com/jetpack/androidx/releases/compose-material3
+        set("material3_version", "1.3.0")
+    }
+}
+
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+plugins {
+    id("com.android.application") version "8.6.1" apply false
+    id("com.android.library") version "8.6.1" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.23" apply false
+    id("com.google.devtools.ksp") version "1.9.23-1.0.19" apply false
+}
+
+tasks.register<Delete>("clean") {
+    delete(rootProject.buildDir)
+}
