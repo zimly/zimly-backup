7656e08c8d95d6e4b5e3c2c8a1225e76db58390a
Espen Jervidalo <espen.jervidalo@gmail.com>
2024-10-18 10:38:12 +0200

    Bump version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
         applicationId = "io.zeitmaschine.zimzync"
         minSdk = 29
         targetSdk = 35
-        versionCode = 31
+        versionCode = 32
         versionName = "1.4.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
