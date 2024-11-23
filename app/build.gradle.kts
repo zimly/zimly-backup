935b773f734012f67890681e6807b4dd239f2342
Espen Jervidalo <espen.jervidalo@gmail.com>
2024-11-23 12:43:43 +0100

    Bump library version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -117,7 +117,7 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-runtime-compose:$lifecycleVersion")
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:$lifecycleVersion")
     implementation("androidx.activity:activity-compose:1.9.3")
-    implementation("androidx.navigation:navigation-compose:2.8.3")
+    implementation("androidx.navigation:navigation-compose:2.8.4")
     implementation("com.google.android.material:material:1.12.0")
 
     // https://developer.android.com/codelabs/android-workmanager#2
