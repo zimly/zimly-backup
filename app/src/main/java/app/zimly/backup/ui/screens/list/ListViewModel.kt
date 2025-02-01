f3ff4d89cee97daa08902228b38b08338e00adca
Espen Jervidalo <espen.jervidalo@gmail.com>
2025-02-01 15:21:11 +0100

    Add source icon to list view #16

--- app/src/main/java/app/zimly/backup/ui/screens/list/ListViewModel.kt
+++ app/src/main/java/app/zimly/backup/ui/screens/list/ListViewModel.kt
@@ -18,7 +18,7 @@ class ListViewModel(private val dataStore: RemoteDao) : ViewModel() {
     // Combines remotes from DB with the selected state into a specialized
     // list of RemoteView data objects
     val remotesState = combine(_remotes, _selectedState)
-    { remotes, sel -> remotes.map { RemoteView(it.uid!!, it.name, it.url, sel.contains(it.uid)) } }
+    { remotes, sel -> remotes.map { RemoteView(it.uid!!, it.name, it.url, it.sourceType, sel.contains(it.uid)) } }
 
     // Notification displayed in the SnackBar upon successful copy/delete operations.
     private val _notification = MutableStateFlow<String?>(null)
