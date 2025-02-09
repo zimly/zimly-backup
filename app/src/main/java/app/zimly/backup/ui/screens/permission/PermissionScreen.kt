8d5a2823efcd081792470c7f9f231ecadb76f8fe
Espen Jervidalo <espen.jervidalo@gmail.com>
2025-02-09 17:28:08 +0100

    Build Permission Composable with actionable to open the app settings #24

--- app/src/main/java/app/zimly/backup/ui/screens/permission/PermissionScreen.kt
+++ app/src/main/java/app/zimly/backup/ui/screens/permission/PermissionScreen.kt
@@ -2,16 +2,35 @@ package app.zimly.backup.ui.screens.permission
 
 import android.Manifest
 import android.app.Application
+import android.content.Intent
 import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Build
+import android.provider.Settings
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.SideEffect
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 import androidx.core.content.ContextCompat
 import androidx.lifecycle.AndroidViewModel
+import app.zimly.backup.ZimlyApplication
 import app.zimly.backup.ui.screens.permission.PermissionViewModel.Companion.getPermissions
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
@@ -38,6 +57,7 @@ class PermissionViewModel(application: Application) : AndroidViewModel(applicati
     }
 
     fun onGranted(grants: Map<String, @JvmSuppressWildcards Boolean>) {
+
         // Lookup and compare the granted permission
         val granted = checkUserGrants(grants)
         if (granted == true) {
@@ -47,7 +67,15 @@ class PermissionViewModel(application: Application) : AndroidViewModel(applicati
         }
     }
 
-    data class UiState (
+    fun openSettings() {
+        val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS).apply {
+            data = Uri.fromParts("package", getApplication<ZimlyApplication>().packageName, null)
+        }
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK)
+        getApplication<ZimlyApplication>().applicationContext.startActivity(intent)
+    }
+
+    data class UiState(
         val granted: Boolean = false,
         val error: String? = null
     )
@@ -80,7 +108,6 @@ class PermissionViewModel(application: Application) : AndroidViewModel(applicati
                 .map { permission -> grants[permission] }
                 .reduce { granted, permission -> granted == true && permission == true }
         }
-
     }
 }
 
@@ -94,15 +121,84 @@ fun PermissionScreen(viewModel: PermissionViewModel) {
 
     val state = viewModel.state.collectAsState()
 
-    if (!state.value.granted) {
-
-        state.value.error?.let {
-            Text(it)
+    Scaffold(
+        bottomBar = {
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(all = 32.dp),
+                horizontalArrangement = Arrangement.Center
+            ) {
+                Button(
+                    onClick = { viewModel.openSettings() },
+                    contentPadding = PaddingValues(horizontal = 74.dp, vertical = 12.dp),
+                    colors = ButtonDefaults.buttonColors(disabledContainerColor = MaterialTheme.colorScheme.surfaceContainerLow)
+                ) {
+                    Text("Update Permission")
+                }
+            }
         }
-        if (state.value.error == null) {
-            SideEffect {
-                permissionLauncher.launch(
-                    getPermissions()
+    ) { innerPadding ->
+
+        Column(
+            verticalArrangement = Arrangement.SpaceEvenly,
+            modifier = Modifier.padding(all = 16.dp) then Modifier.padding(
+                top = innerPadding.calculateTopPadding(),
+                bottom = innerPadding.calculateBottomPadding()
+            ) then Modifier
+                .fillMaxWidth()
+                .fillMaxHeight(),
+        ) {
+            if (!state.value.granted) {
+
+                state.value.error?.let {
+                    PermissionsMissing(it)
+                }
+                if (state.value.error == null) {
+                    LaunchedEffect(Unit) {
+                        permissionLauncher.launch(
+                            getPermissions()
+                        )
+                    }
+                }
+            }
+
+        }
+    }
+
+}
+
+@Composable
+fun PermissionsMissing(warning: String) {
+    Scaffold(
+        bottomBar = {
+        }
+    ) { innerPadding ->
+
+        Column(
+            verticalArrangement = Arrangement.SpaceEvenly,
+            modifier = Modifier.padding(all = 16.dp) then Modifier.padding(
+                top = innerPadding.calculateTopPadding(),
+                bottom = innerPadding.calculateBottomPadding()
+            ) then Modifier
+                .fillMaxWidth()
+                .fillMaxHeight(),
+        ) {
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                modifier = Modifier
+                    .fillMaxWidth()
+            ) {
+                Text(
+                    text = "Missing Permissions!",
+                    color = MaterialTheme.colorScheme.error,
+                    fontSize = MaterialTheme.typography.titleLarge.fontSize,
+                    modifier = Modifier.padding(7.dp)
+                )
+                Text(
+                    warning,
+                    textAlign = TextAlign.Center,
+                    modifier = Modifier.padding(7.dp)
                 )
             }
         }
