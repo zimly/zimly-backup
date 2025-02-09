a646d030fb525312a179834c5eeb99df45383665
Espen Jervidalo <espen.jervidalo@gmail.com>
2025-02-09 18:56:02 +0100

    Show more descriptive warning, close app upon Settings launch #24

--- app/src/main/java/app/zimly/backup/MainActivity.kt
+++ app/src/main/java/app/zimly/backup/MainActivity.kt
@@ -4,9 +4,12 @@ import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.viewModels
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelProvider.AndroidViewModelFactory.Companion.APPLICATION_KEY
+import androidx.lifecycle.ViewModelStoreOwner
+import androidx.lifecycle.viewmodel.MutableCreationExtras
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
@@ -44,7 +47,17 @@ class MainActivity : ComponentActivity() {
             finish()
         }
 
-        val permissionViewModel: PermissionViewModel by viewModels()
+        // TODO: really?
+        val viewModelStoreOwner: ViewModelStoreOwner = this
+        val permissionViewModel: PermissionViewModel = ViewModelProvider.create(
+            viewModelStoreOwner,
+            factory = PermissionViewModel.Factory,
+            extras = MutableCreationExtras().apply {
+                set(APPLICATION_KEY, application)
+                set(PermissionViewModel.CALLBACK_KEY) { finish() }
+            },
+        )[PermissionViewModel::class]
+
         setContent {
             ZimzyncTheme {
                 AppNavigation(remoteDao, permissionViewModel)
@@ -116,8 +129,6 @@ class MainActivity : ComponentActivity() {
             }
         }
     }
-
-
 
 
 }
--- app/src/main/java/app/zimly/backup/ui/screens/permission/PermissionScreen.kt
+++ app/src/main/java/app/zimly/backup/ui/screens/permission/PermissionScreen.kt
@@ -16,8 +16,12 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Check
+import androidx.compose.material.icons.filled.Error
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -30,6 +34,11 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.core.content.ContextCompat
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelProvider.AndroidViewModelFactory.Companion.APPLICATION_KEY
+import androidx.lifecycle.viewmodel.CreationExtras
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
 import app.zimly.backup.ZimlyApplication
 import app.zimly.backup.ui.screens.permission.PermissionViewModel.Companion.getPermissions
 import kotlinx.coroutines.flow.MutableStateFlow
@@ -37,7 +46,8 @@ import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
 
-class PermissionViewModel(application: Application) : AndroidViewModel(application) {
+class PermissionViewModel(private val application: Application, private val closeActivity: () -> Unit) :
+    AndroidViewModel(application) {
 
     private val _state = MutableStateFlow(UiState(granted = isPermissionGranted()))
     val state: StateFlow<UiState> = _state.asStateFlow()
@@ -56,14 +66,14 @@ class PermissionViewModel(application: Application) : AndroidViewModel(applicati
         return granted
     }
 
-    fun onGranted(grants: Map<String, @JvmSuppressWildcards Boolean>) {
+    fun onGranted(grants: Map<String, Boolean>) {
 
         // Lookup and compare the granted permission
         val granted = checkUserGrants(grants)
         if (granted == true) {
             _state.update { it.copy(granted = true) }
         } else {
-            _state.update { it.copy(granted = false, error = "Permissions needed!") }
+            _state.update { it.copy(granted = false, error = true, grants = grants) }
         }
     }
 
@@ -71,16 +81,30 @@ class PermissionViewModel(application: Application) : AndroidViewModel(applicati
         val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS).apply {
             data = Uri.fromParts("package", getApplication<ZimlyApplication>().packageName, null)
         }
-        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK)
-        getApplication<ZimlyApplication>().applicationContext.startActivity(intent)
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_CLEAR_TOP)
+        application.applicationContext.startActivity(intent)
+        // Don't send user back to permission-screen
+        closeActivity()
     }
 
     data class UiState(
         val granted: Boolean = false,
-        val error: String? = null
+        val error: Boolean = false,
+        val grants: Map<String, Boolean> = emptyMap()
     )
 
     companion object {
+        val CALLBACK_KEY = object : CreationExtras.Key<() -> Unit> {}
+
+        val Factory: ViewModelProvider.Factory = viewModelFactory {
+            initializer {
+                val application = this[APPLICATION_KEY] as ZimlyApplication
+                val callBack = this[CALLBACK_KEY] as () -> Unit
+
+                PermissionViewModel(application, callBack)
+            }
+        }
+
         /*
          * Returns the needed permissions based on Android SDK version.
          * Changes were introduced to the media permissions in API 33+
@@ -123,18 +147,20 @@ fun PermissionScreen(viewModel: PermissionViewModel) {
 
     Scaffold(
         bottomBar = {
-            Row(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(all = 32.dp),
-                horizontalArrangement = Arrangement.Center
-            ) {
-                Button(
-                    onClick = { viewModel.openSettings() },
-                    contentPadding = PaddingValues(horizontal = 74.dp, vertical = 12.dp),
-                    colors = ButtonDefaults.buttonColors(disabledContainerColor = MaterialTheme.colorScheme.surfaceContainerLow)
+            if (state.value.error) {
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(all = 32.dp),
+                    horizontalArrangement = Arrangement.Center
                 ) {
-                    Text("Update Permission")
+                    Button(
+                        onClick = { viewModel.openSettings() },
+                        contentPadding = PaddingValues(horizontal = 74.dp, vertical = 12.dp),
+                        colors = ButtonDefaults.buttonColors(disabledContainerColor = MaterialTheme.colorScheme.surfaceContainerLow)
+                    ) {
+                        Text("Update Permissions")
+                    }
                 }
             }
         }
@@ -151,10 +177,10 @@ fun PermissionScreen(viewModel: PermissionViewModel) {
         ) {
             if (!state.value.granted) {
 
-                state.value.error?.let {
-                    PermissionsMissing(it)
+                if (state.value.grants.isNotEmpty()) {
+                    PermissionsMissing(state.value.grants)
                 }
-                if (state.value.error == null) {
+                if (!state.value.error) {
                     LaunchedEffect(Unit) {
                         permissionLauncher.launch(
                             getPermissions()
@@ -169,7 +195,7 @@ fun PermissionScreen(viewModel: PermissionViewModel) {
 }
 
 @Composable
-fun PermissionsMissing(warning: String) {
+fun PermissionsMissing(grants: Map<String, Boolean>) {
     Scaffold(
         bottomBar = {
         }
@@ -196,10 +222,29 @@ fun PermissionsMissing(warning: String) {
                     modifier = Modifier.padding(7.dp)
                 )
                 Text(
-                    warning,
+                    "Zimly needs the following permissions to function properly",
                     textAlign = TextAlign.Center,
                     modifier = Modifier.padding(7.dp)
                 )
+                grants.map {
+                    Row(
+                        modifier = Modifier.fillMaxWidth(),
+                        horizontalArrangement = Arrangement.SpaceBetween
+                    ) {
+                        Text(text = it.key.substringAfterLast("."))
+                        if (it.value) {
+                            Icon(
+                                imageVector = Icons.Default.Check,
+                                "Permission check"
+                            )
+                        } else {
+                            Icon(
+                                imageVector = Icons.Default.Error,
+                                "Permission fail",
+                            )
+                        }
+                    }
+                }
             }
         }
     }
