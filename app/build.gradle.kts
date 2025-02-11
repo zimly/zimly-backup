cae20b1d04fa82048bdea7d04255505e8d73ea82
Espen Jervidalo <espen.jervidalo@gmail.com>
2025-02-11 18:05:56 +0100

    Bump version, update release notes

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,7 +15,7 @@ android {
         namespace = "app.zimly.backup"
         minSdk = 29
         targetSdk = 35
-        versionCode = 55
+        versionCode = 56
         versionName = "2.1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
