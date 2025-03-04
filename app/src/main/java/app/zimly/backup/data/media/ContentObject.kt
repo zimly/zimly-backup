3706f819042c357df078da1d23e4b9ae20e8e38e
Espen Jervidalo <espen.jervidalo@gmail.com>
2025-03-04 18:33:28 +0100

    QA Javadoc

--- app/src/main/java/app/zimly/backup/data/media/ContentObject.kt
+++ app/src/main/java/app/zimly/backup/data/media/ContentObject.kt
@@ -2,6 +2,9 @@ package app.zimly.backup.data.media
 
 import android.net.Uri
 
+/**
+ * Represents a local document or media object.
+ */
 data class ContentObject(
     var name: String,
     var size: Long,
