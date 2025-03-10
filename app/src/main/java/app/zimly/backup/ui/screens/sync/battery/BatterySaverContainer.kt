bb718bebb9ed829433e6dd5779c6845744b94634
Espen Jervidalo <espen.jervidalo@gmail.com>
2025-03-10 14:10:58 +0100

    Implement checkbox for ignoring future battery warnings #27

--- app/src/main/java/app/zimly/backup/ui/screens/sync/battery/BatterySaverContainer.kt
+++ app/src/main/java/app/zimly/backup/ui/screens/sync/battery/BatterySaverContainer.kt
@@ -2,7 +2,10 @@ package app.zimly.backup.ui.screens.sync.battery
 
 import android.content.Context
 import android.content.Intent
+import android.os.BatteryManager
+import android.os.PowerManager
 import android.provider.Settings
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -12,14 +15,16 @@ import androidx.compose.material.icons.outlined.BatteryAlert
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Checkbox
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
@@ -29,25 +34,58 @@ import androidx.compose.ui.unit.dp
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.ViewModelProvider.AndroidViewModelFactory.Companion.APPLICATION_KEY
-import androidx.lifecycle.compose.LocalLifecycleOwner
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import androidx.lifecycle.viewModelScope
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
+import androidx.room.Room
+import app.zimly.backup.data.db.ZimlyDatabase
+import app.zimly.backup.data.db.notification.Notification
+import app.zimly.backup.data.db.notification.NotificationDao
+import app.zimly.backup.data.db.notification.NotificationType
 import app.zimly.backup.ui.theme.containerBackground
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
 
-class BatterySaverViewModel(private val powerStatus: PowerStatusProvider) : ViewModel() {
+class BatterySaverViewModel(
+    private val powerStatus: PowerStatusProvider,
+    private val notificationDao: NotificationDao
+) : ViewModel() {
 
     private val _showWarning: MutableStateFlow<Boolean> = MutableStateFlow(false)
     val showWarning: StateFlow<Boolean> = _showWarning.asStateFlow()
     private val _showDialog: MutableStateFlow<Boolean> = MutableStateFlow(false)
     val showDialog: StateFlow<Boolean> = _showDialog.asStateFlow()
 
-    fun updateBatteryState() {
-        _showWarning.value = !(powerStatus.isBatterSaverDisabled() || powerStatus.isCharging())
+    init {
+        viewModelScope.launch {
+            val batteryWarning = !(powerStatus.isBatterSaverDisabled() || powerStatus.isCharging())
+            _showWarning.value = batteryWarning && !isIgnoreWarning()
+        }
+    }
+
+    private suspend fun isIgnoreWarning(): Boolean {
+        val read = notificationDao.loadByType(NotificationType.BATTERY_SAVER_WARNING)?.ignore ?: false
+        return read
+    }
+
+    private suspend fun setIgnoreWarning() {
+        val notification = notificationDao.loadByType(NotificationType.BATTERY_SAVER_WARNING)
+        if (notification == null) {
+            notificationDao.insert(
+                Notification(
+                    uid = null,
+                    type = NotificationType.BATTERY_SAVER_WARNING,
+                    ignore = true
+                )
+            )
+        } else {
+            val updated = Notification(notification.uid, notification.type, true)
+            notificationDao.update(updated)
+        }
     }
 
     fun openSettings(context: Context) {
@@ -61,8 +99,12 @@ class BatterySaverViewModel(private val powerStatus: PowerStatusProvider) : View
         _showDialog.value = true
     }
 
-    fun closeDialog() {
+    suspend fun closeDialog(ignore: Boolean) {
         _showDialog.value = false
+        _showWarning.value = false
+        if (ignore) {
+            setIgnoreWarning()
+        }
     }
 
     companion object {
@@ -70,17 +112,24 @@ class BatterySaverViewModel(private val powerStatus: PowerStatusProvider) : View
         val Factory: ViewModelProvider.Factory = viewModelFactory {
             initializer {
                 val application = checkNotNull(this[APPLICATION_KEY])
+                val db = Room.databaseBuilder(
+                    application.applicationContext,
+                    ZimlyDatabase::class.java,
+                    "zim-db"
+                )
+                    .build()
+                val notificationDao = db.notificationDao()
 
-                val powerManager = application.getSystemService(android.os.PowerManager::class.java)
+                val powerManager = application.getSystemService(PowerManager::class.java)
                 val batteryManager =
-                    application.getSystemService(android.os.BatteryManager::class.java)
+                    application.getSystemService(BatteryManager::class.java)
                 val powerStatus = PowerStatusProviderImpl(
                     powerManager,
                     batteryManager,
                     application.applicationContext.packageName
                 )
 
-                BatterySaverViewModel(powerStatus)
+                BatterySaverViewModel(powerStatus, notificationDao)
             }
         }
     }
@@ -90,20 +139,16 @@ class BatterySaverViewModel(private val powerStatus: PowerStatusProvider) : View
 fun BatterySaverContainer(viewModel: BatterySaverViewModel = viewModel(factory = BatterySaverViewModel.Factory)) {
 
     val context = LocalContext.current.applicationContext
-    val lifecycleOwner = LocalLifecycleOwner.current
-    val state by lifecycleOwner.lifecycle.currentStateFlow.collectAsState()
     val showWarning by viewModel.showWarning.collectAsStateWithLifecycle()
     val showDialog by viewModel.showDialog.collectAsStateWithLifecycle()
-
-    LaunchedEffect(state) {
-        viewModel.updateBatteryState()
-    }
 
     if (showWarning)
         BatterySaverWarning { viewModel.openDialog() }
 
     if (showDialog)
-        BatterySaverInfoDialog({ viewModel.closeDialog() }, { viewModel.openSettings(context) })
+        BatterySaverInfoDialog(
+            { viewModel.viewModelScope.launch { viewModel.closeDialog(it) } },
+            { viewModel.openSettings(context) })
 }
 
 @Composable
@@ -142,9 +187,10 @@ private fun BatterySaverWarning(openSettings: () -> Unit) {
 
 @Composable
 fun BatterySaverInfoDialog(
-    closeDialog: () -> Unit,
+    closeDialog: (ignore: Boolean) -> Unit,
     openSettings: () -> Unit,
 ) {
+    var checked by remember { mutableStateOf(false) }
     AlertDialog(
         icon = {
             Icon(
@@ -157,20 +203,35 @@ fun BatterySaverInfoDialog(
             Text(text = "Battery Saver Detected")
         },
         text = {
-            Text(
-                text = buildAnnotatedString {
-                    append("Battery Saver is enabled, which may interrupt background synchronization.")
-                    append("\n\nTo prevent this you can either:\n")
-                    append("  • Plug in the charger\n")
-                    append("  • Disable Battery Saver for Zimly")
-                },
-                textAlign = TextAlign.Left,
-                modifier = Modifier.padding(start = 12.dp)
+            Column {
 
-            )
+                Text(
+                    text = buildAnnotatedString {
+                        append("Battery Saver is enabled, which may interrupt background synchronization.")
+                        append("\n\nTo prevent this you can either:\n")
+                        append("  • Plug in the charger\n")
+                        append("  • Disable Battery Saver for Zimly")
+                    },
+                    textAlign = TextAlign.Left,
+                    modifier = Modifier.padding(start = 12.dp)
+
+                )
+
+                Row(
+                    verticalAlignment = Alignment.CenterVertically,
+                ) {
+                    Checkbox(
+                        checked = checked,
+                        onCheckedChange = { checked = it }
+                    )
+                    Text(
+                        "Don't show again"
+                    )
+                }
+            }
         },
         onDismissRequest = {
-            closeDialog()
+            closeDialog(checked)
         },
         confirmButton = {
             TextButton(onClick = { openSettings() }) {
@@ -178,7 +239,7 @@ fun BatterySaverInfoDialog(
             }
         },
         dismissButton = {
-            TextButton(onClick = { closeDialog() }
+            TextButton(onClick = { closeDialog(checked) }
             ) {
                 Text("Dismiss")
             }
