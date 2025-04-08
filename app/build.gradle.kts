9b2e22f126e3393f4996d5b5f388f2879c5983ee
Espen Jervidalo <espen.jervidalo@gmail.com>
2025-04-08 19:23:37 +0200

    Bump versions

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -141,13 +141,13 @@ dependencies {
     // To use Kotlin annotation processing tool (ksp)
     ksp("androidx.room:room-compiler:$roomVersion")
 
-    testImplementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.8.0")
+    testImplementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.8.1")
     testImplementation("junit:junit:4.13.2")
     testImplementation("org.testcontainers:minio:1.20.6")
     testImplementation("com.squareup.okhttp3:mockwebserver:4.12.0")
     testImplementation("io.mockk:mockk:1.13.17")
     testImplementation("androidx.test:core-ktx:1.6.1")
-    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.10.1")
+    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.10.2")
 
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
 
