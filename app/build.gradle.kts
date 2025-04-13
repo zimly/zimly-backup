a1571fda69be5eae64511690524b01c89eb3721c
Espen Jervidalo <espen.jervidalo@gmail.com>
2025-04-13 19:18:15 +0200

    Prepare release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,7 +17,7 @@ android {
         namespace = "app.zimly.backup"
         minSdk = 29
         targetSdk = 35
-        versionCode = 71
+        versionCode = 72
         versionName = "2.4.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
