c4dcd93267d7f49c29b946bf6869e8b5c086411d
Espen Jervidalo <espen.jervidalo@gmail.com>
2025-05-25 11:43:28 +0200

    Bump libraries

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -95,7 +95,7 @@ dependencies {
 
     // https://developer.android.com/develop/ui/compose/bom
     // Not very happy with this, feels like it's not fully thought through, e.g. androidx dependencies.
-    val composeBom = platform("androidx.compose:compose-bom:2025.04.00")
+    val composeBom = platform("androidx.compose:compose-bom:2025.05.01")
     implementation(composeBom)
     testImplementation(composeBom)
     androidTestImplementation(composeBom)
@@ -103,7 +103,7 @@ dependencies {
 
     val workManagerVersion = "2.10.1"
     val roomVersion = "2.7.1"
-    val lifecycleVersion = "2.8.7"
+    val lifecycleVersion = "2.9.0"
 
     // minio / S3
     implementation("io.minio:minio:8.5.17")
@@ -131,7 +131,7 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-runtime-compose:$lifecycleVersion")
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:$lifecycleVersion")
     implementation("androidx.activity:activity-compose:1.10.1")
-    implementation("androidx.navigation:navigation-compose:2.8.9")
+    implementation("androidx.navigation:navigation-compose:2.9.0")
 
     implementation("androidx.documentfile:documentfile:1.1.0")
 
@@ -147,9 +147,9 @@ dependencies {
 
     testImplementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.8.1")
     testImplementation("junit:junit:4.13.2")
-    testImplementation("org.testcontainers:minio:1.20.6")
+    testImplementation("org.testcontainers:minio:1.21.0")
     testImplementation("com.squareup.okhttp3:mockwebserver:4.12.0")
-    testImplementation("io.mockk:mockk:1.14.0")
+    testImplementation("io.mockk:mockk:1.14.2")
     testImplementation("androidx.test:core-ktx:1.6.1")
     testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.10.2")
     testImplementation(kotlin("test"))
@@ -160,7 +160,7 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
     androidTestImplementation("androidx.work:work-testing:$workManagerVersion")
     androidTestImplementation("androidx.test:rules:1.6.1")
-    androidTestImplementation("io.mockk:mockk-android:1.14.0")
+    androidTestImplementation("io.mockk:mockk-android:1.14.2")
 
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
