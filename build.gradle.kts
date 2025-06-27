8704280db8fc87a201797c053cf126041e36b515
Espen Jervidalo <espen.jervidalo@gmail.com>
2025-06-27 17:15:42 +0200

    Upgrade kotlin plugins

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,9 +3,9 @@
 plugins {
     id("com.android.application") version "8.11.0" apply false
     id("com.android.library") version "8.11.0" apply false
-    id("org.jetbrains.kotlin.android") version "2.0.21" apply false
-    id("org.jetbrains.kotlin.plugin.compose") version "2.0.21" apply false
-    id("com.google.devtools.ksp") version "2.0.21-1.0.27" apply false
+    id("org.jetbrains.kotlin.android") version "2.2.0" apply false
+    id("org.jetbrains.kotlin.plugin.compose") version "2.2.0" apply false
+    id("com.google.devtools.ksp") version "2.2.0-2.0.2" apply false
 }
 
 tasks.register<Delete>("clean") {
