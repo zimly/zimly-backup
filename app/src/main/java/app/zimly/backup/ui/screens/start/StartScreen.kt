b855b4d02d4e2addc975da38a5332680068ef5fd
Espen Jervidalo <espen.jervidalo@gmail.com>
2025-07-19 11:37:28 +0200

    Use ListItem in Remote list view instead of custom composable #43

--- app/src/main/java/app/zimly/backup/ui/screens/start/StartScreen.kt
+++ app/src/main/java/app/zimly/backup/ui/screens/start/StartScreen.kt
@@ -1,27 +1,24 @@
 package app.zimly.backup.ui.screens.start
 
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
 import androidx.compose.foundation.combinedClickable
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.displayCutout
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.windowInsetsPadding
-import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.ArrowBack
@@ -30,11 +27,14 @@ import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material.icons.outlined.Cloud
 import androidx.compose.material.icons.outlined.Folder
 import androidx.compose.material.icons.outlined.Image
+import androidx.compose.material3.Card
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FabPosition
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.ListItem
+import androidx.compose.material3.ListItemDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SnackbarDuration
@@ -52,13 +52,12 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.platform.LocalUriHandler
 import androidx.compose.ui.platform.testTag
+import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewModelScope
 import androidx.lifecycle.viewmodel.compose.viewModel
@@ -224,50 +223,44 @@ private fun RemoteItemCard(
     select: (Int) -> Unit,
     syncRemote: (Int, SyncDirection) -> Unit
 ) {
-    Box(
-        modifier = Modifier
-            // Note: Order matters!
-            .clip(RoundedCornerShape(12.dp))
-            .background(color = containerBackground())
-            .then(
-                if (remote.selected) Modifier.border(
-                    width = Dp(2f),
-                    color = MaterialTheme.colorScheme.secondary,
-                    shape = RoundedCornerShape(12.dp)
-                ) else Modifier
-            )
-            .fillMaxWidth()
-            .combinedClickable(
-                onClick = {
-                    if (selectMode) select(remote.uid) else syncRemote(
-                        remote.uid, remote.direction
-                    )
-                },
-                onLongClick = { select(remote.uid) })
-            .padding(16.dp)
+    Card(
+        border = if (remote.selected) BorderStroke(
+            2.dp, MaterialTheme.colorScheme.secondary
+        ) else null
     ) {
-        Row(
-            horizontalArrangement = Arrangement.spacedBy(16.dp),
-            verticalAlignment = Alignment.CenterVertically
-        ) {
-            Column(modifier = Modifier.weight(1f)) {
-                Text(remote.name, color = MaterialTheme.colorScheme.onSurface)
-                Text(remote.url, color = MaterialTheme.colorScheme.onSurfaceVariant)
-            }
-            Column(modifier = Modifier.wrapContentWidth()) {
-                Box(contentAlignment = Alignment.TopEnd) {
-                    val icon = when (remote.direction) {
-                        SyncDirection.UPLOAD -> when (remote.contentType) {
-                            ContentType.MEDIA -> Icons.Outlined.Image
-                            ContentType.FOLDER -> Icons.Outlined.Folder
-                        }
-
-                        SyncDirection.DOWNLOAD -> Icons.Outlined.Cloud
+        ListItem(
+            modifier = Modifier
+                .combinedClickable(
+                    onClick = {
+                        if (selectMode) select(remote.uid) else syncRemote(
+                            remote.uid, remote.direction
+                        )
+                    },
+                    onLongClick = { select(remote.uid) })
+                .semantics(mergeDescendants = true) { },
+            headlineContent = { Text(remote.name) },
+            supportingContent = {
+                Text(
+                    remote.url,
+                    // https://issuetracker.google.com/issues/305342674
+                    // no-wrap as workaround.
+                    maxLines = 1,
+                    overflow = TextOverflow.Ellipsis
+                )
+            },
+            trailingContent = {
+                val icon = when (remote.direction) {
+                    SyncDirection.UPLOAD -> when (remote.contentType) {
+                        ContentType.MEDIA -> Icons.Outlined.Image
+                        ContentType.FOLDER -> Icons.Outlined.Folder
                     }
-                    Icon(icon, "Remote Configuration")
+
+                    SyncDirection.DOWNLOAD -> Icons.Outlined.Cloud
                 }
-            }
-        }
+                Icon(icon, "Remote Configuration")
+            },
+            colors = ListItemDefaults.colors(containerColor = containerBackground())
+        )
     }
 }
 
@@ -304,7 +297,7 @@ fun DefaultPreview() {
             RemoteItemState(
                 uid = it,
                 name = "test $it",
-                url = "https://blob.rawbot.zone/$it",
+                url = if (it % 3 == 0) "https://blob.rawbot.zone/$it/very-long-url-that-should-wrap-or-so " else "https://blob.rawbot.zone/$it",
                 contentType = if (it % 2 == 0) ContentType.MEDIA else ContentType.FOLDER,
                 direction = SyncDirection.UPLOAD
             )
