5be4022c005d88297ed33035dda7e674ab24aafd
Espen Jervidalo <espen.jervidalo@gmail.com>
2025-08-30 15:28:51 +0200

    Bump versions

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -95,15 +95,15 @@ dependencies {
 
     // https://developer.android.com/develop/ui/compose/bom
     // Not very happy with this, feels like it's not fully thought through, e.g. androidx dependencies.
-    val composeBom = platform("androidx.compose:compose-bom:2025.06.01")
+    val composeBom = platform("androidx.compose:compose-bom:2025.08.01")
     implementation(composeBom)
     testImplementation(composeBom)
     androidTestImplementation(composeBom)
     debugImplementation(platform(composeBom))
 
-    val workManagerVersion = "2.10.2"
+    val workManagerVersion = "2.10.3"
     val roomVersion = "2.7.2"
-    val lifecycleVersion = "2.9.1"
+    val lifecycleVersion = "2.9.3"
 
     // minio / S3
     implementation("io.minio:minio:8.5.17")
@@ -116,7 +116,7 @@ dependencies {
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.18.2")
     implementation("javax.xml.stream:stax-api:1.0-2")
 
-    implementation("androidx.core:core-ktx:1.16.0")
+    implementation("androidx.core:core-ktx:1.17.0")
 
     // Managed dependencies
     implementation("androidx.compose.material3:material3")
@@ -131,7 +131,7 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-runtime-compose:$lifecycleVersion")
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:$lifecycleVersion")
     implementation("androidx.activity:activity-compose:1.10.1")
-    implementation("androidx.navigation:navigation-compose:2.9.1")
+    implementation("androidx.navigation:navigation-compose:2.9.3")
 
     implementation("androidx.documentfile:documentfile:1.1.0")
 
@@ -145,23 +145,23 @@ dependencies {
     // To use Kotlin annotation processing tool (ksp)
     ksp("androidx.room:room-compiler:$roomVersion")
 
-    testImplementation("org.robolectric:robolectric:4.15.1")
+    testImplementation("org.robolectric:robolectric:4.16")
     testImplementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.9.0")
     testImplementation("junit:junit:4.13.2")
     testImplementation("org.testcontainers:minio:1.21.3")
-    testImplementation("com.squareup.okhttp3:mockwebserver:5.0.0")
-    testImplementation("io.mockk:mockk:1.14.4")
-    testImplementation("androidx.test:core-ktx:1.6.1")
+    testImplementation("com.squareup.okhttp3:mockwebserver:5.1.0")
+    testImplementation("io.mockk:mockk:1.14.5")
+    testImplementation("androidx.test:core-ktx:1.7.0")
     testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.10.2")
-    testImplementation("org.jetbrains.kotlin:kotlin-test:2.2.0")
+    testImplementation("org.jetbrains.kotlin:kotlin-test:2.2.10")
 
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
 
     // https://developer.android.com/jetpack/androidx/releases/test
-    androidTestImplementation("androidx.test.ext:junit:1.2.1")
+    androidTestImplementation("androidx.test.ext:junit:1.3.0")
     androidTestImplementation("androidx.work:work-testing:$workManagerVersion")
-    androidTestImplementation("androidx.test:rules:1.6.1")
-    androidTestImplementation("io.mockk:mockk-android:1.14.4")
+    androidTestImplementation("androidx.test:rules:1.7.0")
+    androidTestImplementation("io.mockk:mockk-android:1.14.5")
 
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
